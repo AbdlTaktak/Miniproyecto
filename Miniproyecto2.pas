b2b568a3f4448812aa14f23a2ce1d51a2d0904a4
Abdl <ataktak.1549@unimar.edu.ve>
2023-03-14 19:31:53 -0400

    empezando el programaga

--- Miniproyecto2.pas
+++ Miniproyecto2.pas
@@ -2,9 +2,15 @@
 program ejercicio2;
 
 uses crt;
-var i : byte;
+var 
+// agregando una variable//
+num1 : integer;
 
 BEGIN
+	//inicindo el codigo//
+	writeln('Bienvenido sea');
+	write('Ingrese un numero mayor a cero para saber en que color se va a imprimir:');
+	readln(num1);
 	
 	
 END.
