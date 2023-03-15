f508af3b81b625581682ab51a6d3717410e53408
Abdl <ataktak.1549@unimar.edu.ve>
2023-03-14 20:29:37 -0400

    iniciando el segundo ejercicio

--- Miniproyecto2.pas
+++ Miniproyecto2.pas
@@ -7,7 +7,7 @@ var
 num1 : integer;
 
 BEGIN
-	//inicindo el codigo//
+	//inicindo el ejercicio 2//
 	writeln('Bienvenido sea');
 	write('Ingrese un numero mayor a cero para saber en que color se va a imprimir:');
 	readln(num1);
