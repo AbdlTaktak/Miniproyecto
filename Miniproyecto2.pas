3a30d94fa380a3f9b849ef970e491a450fe1999a
Abdl <ataktak.1549@unimar.edu.ve>
2023-03-14 22:11:00 -0400

    agregando diferentes criterios de divisibilidad

--- Miniproyecto2.pas
+++ Miniproyecto2.pas
@@ -18,5 +18,50 @@ BEGIN
 	readln(num1);
 	end;
 	
+	//introduccion de los diferentes criterios de divisibilidad usando como valores los dados en el enunciado (2,3,5,7,13)//
+	if num1 mod 2=0 then 
+	begin 
+		textcolor(15);
+		writeln('');
+		writeln('----Su valor es divisible entre 2:'); textcolor(14);
+		write('[',num1,']');
+		writeln('');
+	end;
+	
+		if num1 mod 3=0 then 
+			begin 
+				textcolor(15);
+				writeln('');
+				writeln('----Su valor es divisible entre 3:'); textcolor(1);
+				write('[',num1,']');
+				writeln('');
+		end;
+		
+				if num1 mod 5=0 then
+					begin 
+						textcolor(15);
+						writeln('');
+						writeln('----Su valor es divisible entre 5:'); textcolor(4);
+						writeln('[',num1,']');
+						writeln('');
+					end;
+				
+					 if num1 mod 7=0 then
+						begin
+							textcolor(15);
+							writeln('');
+							writeln('----Su valor es divisible entre 7:'); textcolor(2);
+							writeln('[',num1,']');
+							writeln('');
+						end;
+					
+						 if num1 mod 13=0 then 
+							begin 
+								textcolor(15);
+								writeln('');
+								writeln('Su valor es divisible entre 13:'); textcolor(6);
+								writeln('[',num1,']');
+								writeln('');
+							end;
 END.
 
