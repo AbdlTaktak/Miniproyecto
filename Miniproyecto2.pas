52717d20ae12047b656afceb0737066e93f4005f
Abdl <ataktak.1549@unimar.edu.ve>
2023-03-15 18:53:28 -0400

    agregando mejoras visuales

--- Miniproyecto2.pas
+++ Miniproyecto2.pas
@@ -15,32 +15,39 @@ num1 : longint;
 			begin
 			WriteLn('su valor es divisible entre 2:');
 			textColor(yellow);WriteLn('[',v1,']');
-			
+			textcolor(white);
+			WriteLn('--------------------------------------');
 			end;
 		
 		if v1 mod 3=0 then
 			begin
 			WriteLn('su valor es divisible entre 3:');
 			textColor(blue);WriteLn('[',v1,']');
-			
+			textcolor(white);
+			WriteLn('--------------------------------------');
 			end; 
 			
 		if v1 mod 5=0 then
 			begin
 			WriteLn('su valor es divisible entre 5:');
 			textColor(red);WriteLn('[',v1,']');
+			textcolor(white);
+			WriteLn('--------------------------------------');
 			end;
 			
 		if v1 mod 7=0 then
 			begin
 			WriteLn('su valor es divisible entre 7:');
 			textColor(green);WriteLn('[',v1,']');
-			
+			textcolor(white);
+			WriteLn('--------------------------------------');
 			end;
 		if v1 mod 13=0 then
 			begin
 			WriteLn('su valor es divisible entre 13:');
 			textColor(brown);WriteLn('[',v1,']');
+			textcolor(white);
+			WriteLn('--------------------------------------');
 			end;
 		//introduciendo las diferentes posibilidades dentro de la funcion//
 					if (v1 mod 2<>0) and (v1 mod 3<>0) and (v1 mod 5<>0) and (v1 mod 7<>0) and (v1 mod 13<>0) then
@@ -53,9 +60,14 @@ BEGIN
 	// en esta parte agregamos la pregunta para ingresar el dato//
 	writeln('ingrese un numero');
 	readln (num1);
+	while num1<=0 do
+	begin
+	writeln('Dato invalido, porfavor escoga un numero positivo y que sea mayor que cero');
+	readln(num1);
+	end;
 	divisibilidad(num1);
 	readln();
-	
-			
+			//agregando sentencia repetitiva para numeros menores he iguales que 0//
+		
 END.
 
